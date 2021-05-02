e126facd83d255d7c4a1cb83a15361f40c97dd0b
kubajabko <kubachochol94@gmail.com>
2021-05-02 14:24:43 +0200

    DB working in a work-around

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,9 @@ import com.example.controllers.EmployeeController;
 import com.example.model.Department;
 import com.example.model.Employee;
 
+import com.example.persistence.HibernateUtil;
+import org.hibernate.Session;
+
 public class App {
 
     //private boolean isOn = true;
@@ -14,11 +17,21 @@ public class App {
         Employee emp2 = new Employee("Jan", "Kowalski", 2);
         Department dep1 = new Department("R&D", 1);
         Department dep2 = new Department("Sales",2);
-        EmployeeController employeeController = new EmployeeController();
-        DepartmentController departmentController = new DepartmentController();
-        employeeController.addEmployeeToDB(emp1);
-        employeeController.addEmployeeToDB(emp2);
-        departmentController.addDepartmentToDB(dep1);
-        departmentController.addDepartmentToDB(dep2);
+//        EmployeeController employeeController = new EmployeeController();
+//        DepartmentController departmentController = new DepartmentController();
+//        employeeController.addEmployeeToDB(emp1);
+//        employeeController.addEmployeeToDB(emp2);
+//        departmentController.addDepartmentToDB(dep1);
+//        departmentController.addDepartmentToDB(dep2);
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        //Save the employee in database
+        session.save(emp1);
+
+        //Commit the transaction
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
     }
 }
